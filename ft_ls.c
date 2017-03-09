4b043670ed14c4d0619557082de61e1404f17128
Erik Williamson <ewilliam@e1z3r5p19.42.us.org>
2017-03-09 10:40:30 -0800

    linked list sandbox working

--- ft_ls.c
+++ ft_ls.c
@@ -6,7 +6,7 @@
 /*   By: ewilliam <erik@himself.in>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/06 11:35:17 by ewilliam          #+#    #+#             */
-/*   Updated: 2017/03/08 12:22:52 by ewilliam         ###   ########.fr       */
+/*   Updated: 2017/03/08 17:01:17 by ewilliam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,139 +21,13 @@ int		ft_errx(int e, const char *s)
 typedef struct			s_output
 {
 	char				name[256];
-	struct s_output		*next;
 	mode_t				mode;
 }						t_output;
 
 typedef struct			s_input
 {
-	struct s_output		*head;
-	struct s_input		*next;
+	struct stat			*stats;
 }						t_input;
-
-typedef struct			s_node
-{
-    int					data;
-    struct s_node*		left;
-    struct s_node*		right;
-}						t_node;
-
-t_node					*create_node(int data)
-{
-	t_node	*new_node;
-
-	if (!(new_node = malloc(sizeof(*new_node))))
-		ft_errx(1, "Unable to allocate memory for node");
-	new_node->data = data;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	return (new_node);
-}
-
-int		compare_int(int left, int right)
-{
-	if (left > right)
-		return (1);
-	if (left < right)
-		return (-1);
-	return (0);
-}
-
-void	display_node(t_node *node)
-{
-	if (node)
-		printf("%d\n", node->data);
-}
-
-void	display_tree(t_node *node)
-{
-	if (!node)
-		return;
-	printf("%d",node->data);
-    if(node->left != NULL)
-        printf("(L:%d)",node->left->data);
-    if(node->right != NULL)
-        printf("(R:%d)",node->right->data);
-    printf("\n");
-	display_tree(node->left);
-	display_tree(node->right);
-}
-
-typedef int (*comparer)(int, int);
-typedef void (*callback)(t_node*);
-
-t_node* insert_node(t_node *root, comparer compare, int data)
-{
-	int		is_left;
-	int		r;
-	t_node	*cursor;
-	t_node	*prev;
-
-	if (!root)
-		root = create_node(data);
-	else
-	{
-		is_left = 0;
-		r = 0;
-		cursor = root;
-		prev = NULL;
-		while (cursor)
-		{
-			r = compare(data, cursor->data);
-			prev = cursor;
-			if (r < 0)
-			{
-				is_left = 1;
-				cursor = cursor->left;
-			}
-			else if (r > 0)
-			{
-				is_left = 0;
-				cursor = cursor->right;
-			}
-		}
-		if (is_left)
-			prev->left = create_node(data);
-		else
-			prev->right = create_node(data);
-	}
-	return (root);
-}
-
-void	traverse(t_node *root, callback cb)
-{
-	t_node	*cursor;
-	t_node	*prev;
-
-	if (!root)
-		return;
-	cursor = root;
-	while (cursor)
-	{
-		if (cursor->left)
-		{
-			cb(cursor);
-			cursor = cursor->right;
-		}
-		else
-		{
-			prev = cursor->left;
-			while (prev->right && prev->right != cursor)
-				prev = prev->right;
-			if (prev->right)
-			{
-				prev->right = cursor;
-				cursor = cursor->left;
-			}
-			else
-			{
-				prev->right = NULL;
-				cb(cursor);
-				cursor = cursor->right;
-			}
-		}
-	}
-}
 
 int		main(int ac, char **av)
 {
@@ -161,16 +35,26 @@ int		main(int ac, char **av)
 	struct dirent	*dptr = NULL;
 	struct stat		*stats = NULL;
 	unsigned short	arg = 1;
+	t_input			*input = NULL;
+	t_list			*lst_input = NULL;
+	t_list			*lst_cursor = NULL;
 
 	stats = ft_memalloc(sizeof(*stats));
+	input = ft_memalloc(sizeof(*input));
+	input->stats = ft_memalloc(sizeof(*stats));
+	lst_input = ft_lstnew(input, sizeof(input));
+	lst_cursor = lst_input;
 	while (arg < ac)
 	{
 		printf("Try open: %s\n", av[arg]);
-		stat(av[arg], stats);
+		stat(av[arg], input->stats);
+		input->stats = stats;
 		printf("%s is %s\n", av[arg], S_ISREG(stats->st_mode) ? "a file" : "not a file");
 		if (!(dirp = opendir(av[arg])))
 			dprintf(2, "%s: No such file or directory\n", av[arg]);
 		++arg;
+		lst_cursor->next = ft_lstnew(NULL, 0);
+		lst_cursor = lst_cursor->next;
 	}
 	while (dirp && (dptr = readdir(dirp)) != NULL)
 	{
@@ -179,19 +63,41 @@ int		main(int ac, char **av)
 	}
 	if (dirp)
 		(void)closedir(dirp);
+//	free(&stats);
+	free(&(input->stats));
+//	free(&input);
 
-
-
-#define SIZE 9
-	int a[SIZE] = {8,3,10,1,6,14,4,7,13};
+	typedef struct	s_int
+	{
+		int			n;
+	}				t_int;
+	
+	#define SIZE 9
 	int i = 0;
-	t_node * root = NULL;
+
+	t_list	*head = NULL;
+	t_list	*cursor = NULL;
+	t_int	*container;
+
 	while (i < SIZE)
 	{
-		root = insert_node(root, compare_int, a[i]);
+		if (cursor)
+		{
+			cursor->next = ft_lstnew(container, sizeof(container));
+			cursor = cursor->next;
+		}
+		else
+		{
+			head = ft_lstnew(container, sizeof(container));
+			cursor = head;
+		}
+		((t_int*)cursor->content)->n = i;
 		i++;
 	}
-	display_tree(root);
-//	traverse(root, display_node);
-
+	cursor = head;
+	while (cursor)
+	{
+		printf("%d\n", ((t_int*)cursor->content)->n);
+		cursor = cursor->next;
+	}
 }
